96c89400179520756ea7cc501638f8b6405a7fec
Manikandan-App-developer <manikandan.iosdev@gmail.com>
2024-05-08 22:48:45 +0530

    longestSubstring

--- SwiftDSA/Longest Substring Without Repeating Characters.swift
+++ SwiftDSA/Longest Substring Without Repeating Characters.swift
@@ -10,56 +10,68 @@ import Foundation
 //let str = "fabacghj".map { $0 }
 //let inputString = "abacabfb"
 
-func longestSubstring(_ s: String) -> String {
-    var longest = ""
-    var characters = Set<Character>()
-    var startIndex = 0
-    var endIndex = 0
-    
-    let sArray = Array(s)
 
-    while endIndex < sArray.count {
-        let char = sArray[endIndex]
-        if characters.contains(char) {
-//           Characters need to be removed by using startIndex
-            characters.remove(sArray[startIndex])
-            startIndex += 1
-        } else {
-            characters.insert(char)
-            endIndex += 1
-            if endIndex - startIndex > longest.count {
-                longest = String(sArray[startIndex..<endIndex])
-            }
-        }
-    }
-    
-    return longest
-}
+//func longestSubstring() {
+//    
+//    let string = "aaassssaaaaaabcdvzdb"
+//    var dict = [Character: Int]()
+//    var longestSequence = 0
+//    var longestStr = ""
+//    
+//    let arr = string.map{$0}
+//    
+//    var startIndex = 0
+//    for (endIndex, value) in string.enumerated() {
+//        if let existingItemIndex = dict[value], existingItemIndex >= startIndex {
+//            startIndex = existingItemIndex + 1
+//        }
+//        
+//        longestSequence = max(longestSequence, endIndex - startIndex + 1)
+//        
+//        if longestSequence > longestStr.count {
+//            longestStr = String(arr[startIndex..<endIndex + 1])
+//        }
+//        
+//        dict[value] = endIndex
+//    }
+//
+//    print(longestSequence)
+//        
+//    print("Longest Substring:", longestStr)
+//}
 
 
-func longSubstring() {
+func longestSubstring() {
     
-    let str = "fabacghj".map { $0 }
-    var longestStr = ""
-    var set = Set<Character>()
+    let string = "aaassssaaaaaabcdvzdb"
+    
+    let arr = string.map{$0}
+    
+    var dict = [Character: Int]()
     
     var start = 0
-    var end = 0
+    var longestStr = ""
+    var longestSequence = 0
     
-    while end < str.count {
+    for(index, value) in string.enumerated() {
         
-        let char = str[end]
-        if set.contains(char) {
-            set.remove(str[start])
-            start += 1
-        } else {
-            set.insert(char)
-            if end - start > longestStr.count {
-                longestStr = String(str[start..<end])
-            }
+        if let existingItemIndex = dict[value] {
+            start = existingItemIndex + 1
         }
+        
+        longestSequence = max(longestSequence, index - start + 1)
+        
+        if longestSequence > longestStr.count {
+            longestStr = String(arr[start..<index + 1])
+        }
+        
+        dict[value] = index
     }
     
+    print(longestSequence)
     print(longestStr)
     
+    
 }
+
+
